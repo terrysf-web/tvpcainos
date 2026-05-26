1f2ddb6abbd405b2578c42d39aa79e75b7faf020
terrysf-web <terrysf@gmail.com>
2026-05-25 22:49:03 -0700

    Add files via upload

---
+++ js/upload.js
@@ -0,0 +1,49 @@
+import { db, storage } from './firebase.js';
+
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const uploadBtn = document.getElementById('uploadBtn');
+
+uploadBtn.addEventListener('click', async () => {
+
+  const title = document.getElementById('songTitle').value;
+  const key = document.getElementById('songKey').value;
+  const file = document.getElementById('pdfFile').files[0];
+
+  if (!file) {
+    alert('Select PDF');
+    return;
+  }
+
+  try {
+
+    const storageRef = ref(storage, `songs/${file.name}`);
+
+    await uploadBytes(storageRef, file);
+
+    const url = await getDownloadURL(storageRef);
+
+    await addDoc(collection(db, 'songs'), {
+      title,
+      key,
+      pdfUrl: url,
+      createdAt: new Date()
+    });
+
+    alert('Upload Complete');
+
+  } catch (err) {
+    console.error(err);
+    alert('Upload Failed');
+  }
+
+});
