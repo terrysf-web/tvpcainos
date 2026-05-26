4b03267f6cfb773e38399e13bb554fc798da919b
terrysf-web <terrysf@gmail.com>
2026-05-25 22:33:23 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 // Firebase를 쓰려면 아래 값을 본인 프로젝트 값으로 바꾸세요.
 // 비워두면 앱은 LocalStorage 모드로 안전하게 동작합니다.
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAzXyQA-BbL_0KsTnukODBfMBkIZINxiNM",
+  authDomain: "tvpcainos.firebaseapp.com",
+  projectId: "tvpcainos",
+  storageBucket: "tvpcainos.firebasestorage.app",
+  messagingSenderId: "721441022829",
+  appId: "1:721441022829:web:45a0ee8fc152090b09f064"
 };
 
 export const collectionName = "ainos_v2_library";
